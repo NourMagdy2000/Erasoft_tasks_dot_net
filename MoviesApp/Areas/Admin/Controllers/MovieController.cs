943a744cef284fcb4f7f722af9895085d8894da6
Nour Magdy <neromagdy2@gmail.com>
2025-11-15 18:44:50 +0200

    new updates

--- MoviesApp/Areas/Admin/Controllers/MovieController.cs
+++ MoviesApp/Areas/Admin/Controllers/MovieController.cs
@@ -3,6 +3,7 @@ using MoviesApp.Models;
 using MoviesApp.Repos;
 using MoviesApp.ViewModels;
 using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace MoviesApp.Areas.Admin.Controllers
 {
@@ -16,7 +17,7 @@ namespace MoviesApp.Areas.Admin.Controllers
         IRepository<Movie_sub_imgs> movie_sub_imgsRepository;
 
 
-        public MovieController(IRepository<Movie> MovieRepository, IRepository<Category>categoryRepository, IRepository<Movie_sub_imgs> movie_sub_imgsRepository) 
+        public MovieController(IRepository<Movie> MovieRepository, IRepository<Category> categoryRepository, IRepository<Movie_sub_imgs> movie_sub_imgsRepository)
         {
             this.MovieRepository = MovieRepository;
             this.CategoryRepository = categoryRepository;
@@ -35,9 +36,9 @@ namespace MoviesApp.Areas.Admin.Controllers
         public async Task<IActionResult> AddMovie()
         {
             var categories = await CategoryRepository.GetAsync();
-            
 
-            return View(new AddMovieVM() { Categories=categories as List<Category>,Movie=new Movie()});
+
+            return View(new AddMovieVM() { Categories = categories as List<Category>, Movie = new Movie() });
         }
         [HttpPost]
         public async Task<IActionResult> AddMovie(Movie Movie, IFormFile File, List<IFormFile> SubImages)
@@ -68,7 +69,7 @@ namespace MoviesApp.Areas.Admin.Controllers
                 Movie.Img = FileName;
             }
 
-           var addedMovie= await MovieRepository.AddAsync(Movie);
+            var addedMovie = await MovieRepository.AddAsync(Movie);
             await MovieRepository.CommitAsync();
             if (SubImages is not null)
             {
@@ -95,63 +96,115 @@ namespace MoviesApp.Areas.Admin.Controllers
                     }
                 }
             }
-                    return RedirectToAction(nameof(Index));
+            return RedirectToAction(nameof(Index));
         }
-        //[HttpGet]
-        //public async Task<IActionResult> UpdateMovie(int id)
-        //{
-        //    var MovieInDB = await MovieRepository.GetOneAsync(c => c.Id == id, trackd: false);
+        [HttpGet]
+        public async Task<IActionResult> UpdateMovie(int id)
+        {
+            var MovieInDB = await MovieRepository.GetOneAsync(c => c.Id == id,includes:[p=>p.movie_Sub_Imgs], trackd: false);
+
+            var categories = await CategoryRepository.GetAsync();
+            ViewBag.CategoryId= MovieInDB.CategoryId;
+            return View(new AddMovieVM() { Categories = categories as List<Category>, Movie =MovieInDB });
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateMovie(Movie Movie, IFormFile File, List<IFormFile> SubImages)
+        {
+
+            var MovieInDB = await MovieRepository.GetOneAsync(c => c.Id == Movie.Id, trackd: false);
 
 
-        //    return View(MovieInDB);
-        //}
-        //[HttpPost]
-        //public async Task<IActionResult> UpdateMovie(UpdateMovieVM updateMovieVM)
-        //{
 
-        //    var MovieInDB = await MovieRepository.GetOneAsync(c => c.Id == updateMovieVM.Id, trackd: false);
+            //if (!ModelState.IsValid)
+            //{
+            //    TempData["Error"] = "Invalid Inputs";
+            //    return View(Movie);
+            //}
+            Movie updatedMovie = new Movie() { Id = Movie.Id, Title = Movie.Title, Description = Movie.Description, Date = Movie.Date, CategoryId = Movie.CategoryId, Price = Movie.Price, Img = Movie.Img, Status=Movie.Status,};
+
+            if (File is not null && File.Length > 0)
+            {
+
+                var FileName = Guid.NewGuid().ToString() + "-" + File.FileName;
+                var wwwRootPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "assets", "files");
+
+                if (!Directory.Exists(wwwRootPath))
+                {
+                    Directory.CreateDirectory(wwwRootPath);
+                }
+
+                var FilePath = Path.Combine(wwwRootPath, FileName);
+                using (var stream = System.IO.File.Create(FilePath))
+                {
+                    File.CopyTo(stream);
+                }
+                updatedMovie.Img = FileName;
+                var oldPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\assets\\files\\", MovieInDB.Img);
+
+                if (System.IO.File.Exists(oldPath))
+                {
+                    System.IO.File.Delete(oldPath);
+                }
+
+            }
+            else { updatedMovie.Img = MovieInDB.Img; }
+
+
+            MovieRepository.Update(updatedMovie);
+            await MovieRepository.CommitAsync();
+
+            if (SubImages is not null)
+            {
+                foreach (var item in SubImages)
+                {
+                    if (item.Length > 0)
+                    {
+                        var SubImageName = Guid.NewGuid().ToString() + "-" + item.FileName;
+                        var SubImagePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\assets\\files\\Movie_sub_imgs", SubImageName);
+                        using (var stream = System.IO.File.Create(SubImagePath))
+                        {
+                            item.CopyTo(stream);
+                        }
+                        var movieSubImage = new Movie_sub_imgs()
+                        {
+                            img = SubImageName,
+                            Movieid = Movie.Id
+                        };
+                        //_context.movieSubImages.Add(movieSubImage);
+                        await movie_sub_imgsRepository.AddAsync(movieSubImage);
+                        //_context.SaveChanges();
+                        await movie_sub_imgsRepository.CommitAsync();
+
+                    }
+                }
+
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        public async Task<IActionResult> DeleteSubImage(int movieId, string img)
+        {
+            //var movieSubImage = _context.movieSubImages.FirstOrDefault(p => p.movieId == movieId && p.Img == img);
+            var movieSubImage = await movie_sub_imgsRepository.GetOneAsync(p => p.Movieid == movieId && p.img == img);
+
+            if (movieSubImage is null)
+                return RedirectToAction("NotFoundPage", "Home");
+            //_context.movieSubImages.Remove(movieSubImage);
+            movie_sub_imgsRepository.Delete(movieSubImage);
+            await movie_sub_imgsRepository.CommitAsync();
+            var oldPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Images\\Movie_sub_imgs", movieSubImage.img);
+            if (System.IO.File.Exists(oldPath))
+            {
+                System.IO.File.Delete(oldPath);
+            }
+
+            //_context.SaveChanges();
+            await movie_sub_imgsRepository.CommitAsync();
 
 
 
-        //    if (!ModelState.IsValid)
-        //    {
-        //        TempData["Error"] = "Invalid Inputs";
-        //        return View(updateMovieVM);
-        //    }
-        //    Movie updatedMovie = new Movie() { Id = updateMovieVM.Id, Name = updateMovieVM.Name, Nantionality = updateMovieVM.Nantionality };
+            return RedirectToAction(nameof(UpdateMovie), new { id = movieId });
 
-        //    if (updateMovieVM.File is not null && updateMovieVM.File.Length > 0)
-        //    {
-
-        //        var FileName = Guid.NewGuid().ToString() + "-" + updateMovieVM.File.FileName;
-        //        var wwwRootPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "assets", "files");
-
-        //        if (!Directory.Exists(wwwRootPath))
-        //        {
-        //            Directory.CreateDirectory(wwwRootPath);
-        //        }
-
-        //        var FilePath = Path.Combine(wwwRootPath, FileName);
-        //        using (var stream = System.IO.File.Create(FilePath))
-        //        {
-        //            updateMovieVM.File.CopyTo(stream);
-        //        }
-        //        updatedMovie.File = FileName;
-        //        var oldPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\assets\\files\\", MovieInDB.File);
-
-        //        if (System.IO.File.Exists(oldPath))
-        //        {
-        //            System.IO.File.Delete(oldPath);
-        //        }
-
-        //    }
-        //    else { updatedMovie.File = MovieInDB.File; }
-
-        //    MovieRepository.Update(updatedMovie);
-        //    await MovieRepository.CommitAsync();
-        //    return RedirectToAction(nameof(Index));
-        //}
-
+        }
         public async Task<IActionResult> DeleteMovie(int id)
         {
             //var movie = _context.movies.Include(p=>p.movieSubImages).FirstOrDefault(c => c.Id == id);
